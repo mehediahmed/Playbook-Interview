0f66173d15f41567f855968143f5b6bf05e09d76
Mehedi Ahmed <mehediahmed2@gmail.com>
2023-07-07 16:40:19 -0400

    Cleaned up visuals

--- Assets/_scripts/Rotate.cs
+++ Assets/_scripts/Rotate.cs
@@ -6,9 +6,12 @@ public class Rotate : ObjectTransformation
 {
   
     public Vector3 AxisOfRotation = Vector3.right;
+    private Vector3 AxisOfRingRotation = Vector3.up;
 
     public override void Transform() 
     {
         ObjectToTranslate.transform.Rotate(AxisOfRotation, ObjectViewer.Instance.clickDistance * 0.0025f);
+        transform.parent.Rotate(AxisOfRingRotation, ObjectViewer.Instance.clickDistance * 0.0025f);
+
     }
 }
